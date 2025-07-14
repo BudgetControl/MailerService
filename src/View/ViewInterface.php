024be3f2805fe95e78dfb170c5e2b973744637ae
Marco De Felice <io@marcodefelice.it>
2025-07-14 17:09:37 +0200

    fix view interface

--- src/View/ViewInterface.php
+++ src/View/ViewInterface.php
@@ -7,25 +7,4 @@ use MLAB\SdkMailer\View\ViewInterface as ViewBase;
 
 interface ViewInterface extends ViewBase
 {
-    /**
-     * Returns the view content as a string.
-     *
-     * @return string
-     */
-    public function view() :string;
-
-    /**
-     * Validates the view.
-     *
-     * @return void
-     */
-    public function validate() :void;
-
-    /**
-     * Renders the view with the given data.
-     *
-     * @param array $data The data to be passed to the view.
-     * @return string The rendered view as a string.
-     */
-    public function render(array $data = []): string;
 }
